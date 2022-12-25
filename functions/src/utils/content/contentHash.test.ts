16ad578ef084f8340d29782e2387e81886b26d80
Ethan Harsh <ethan@ethanharsh.com>
2022-12-25 15:40:25 -0500

    checkContent test

--- functions/src/utils/content/contentHash.test.ts
+++ functions/src/utils/content/contentHash.test.ts
@@ -1,6 +1,11 @@
 import contentHash from "./contentHash";
 import featuredRepos from "./__test-data__/featuredRepos.json";
+import recentArticles from "./__test-data__/recentArticles.json";
 
 test("Should return matching hash from featuredRepos.json", () => {
   expect(contentHash(featuredRepos.data)).toBe(featuredRepos.hash);
+});
+
+test("Should return matching hash from recentArticles.json", () => {
+  expect(contentHash(recentArticles.data)).toBe(recentArticles.hash);
 });
