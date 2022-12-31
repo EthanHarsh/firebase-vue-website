80ff31bdea52b6174aaf5f268b0e52c480dc43f0
Ethan Harsh <ethan@ethanharsh.com>
2022-12-30 22:59:26 -0500

    jest deployment

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
-    "babel.config.js",
+    "jest.config.ts",
   ],
   plugins: [
     "@typescript-eslint",
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,11 +2,6 @@
 import * as functions from "firebase-functions";
 import {checkRssFeed} from "./medium";
 import {checkFeaturedRepos} from "./github";
-import {watchGmailMessages} from "./pubsub";
-// import {auth_init, auth_callback} from "./auth";
 
 exports.checkRssFeed = functions.https.onCall(checkRssFeed);
 exports.checkFeaturedRepos = functions.https.onCall(checkFeaturedRepos);
-exports.watchGmailMessages = functions.pubsub.topic("gmail-watch").onPublish(watchGmailMessages);
-// exports.auth_init = functions.https.onRequest(auth_init);
-// exports.auth_callback = functions.https.onRequest(auth_callback);
